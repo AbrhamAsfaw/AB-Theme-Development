33d5f4dac0f516e894a3e788a1d801b7161b47f7
ABRHAMASFAW2018 <abrhamasfaw2018@gmail.com>
2019-12-19 21:50:16 +0300

    index page Modified

--- index.php
+++ index.php
@@ -1,59 +1,47 @@
 <?php
 /**
  * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package ABTheme
  */
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+get_header(); ?>
+        
+        <h2 class="page-heading">Blogs</h2>
+        
+        <section>
+            
+            <?php
+                while ( have_posts() ) {
+                     the_post();       
+            ?> 
+            <div class="card">
+                <div class="card-image">
+                    <a href="<?php echo the_permalink(); ?>">
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
+                    </a>
+                </div>
+                <div class="card-description">
+                    <a href="<?php the_permalink(); ?>">
+                        <h3><?php the_title(); ?></h3>
+                    </a>
+                    <div class="card-meta">
+                    	Posted by <?php the_author(); ?> on <?php the_time('F j , Y') ?> in
+                    	<a href="#"><?php echo get_the_category_list(', ') ?>	
+                    	</a> 
+                    </div>
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 30); ?>
+                    </p>
+                    <a href="<?php the_permalink(); ?>" class="btn-readmore">Read more</a>
+                </div>
+            </div>
+        <?php }
+            wp_reset_query();
+        ?>
+        
+        </section>
+        <div class="pagination">
+        	<?php echo paginate_links(); ?>
+        </div>
+<?php get_footer(); ?>
