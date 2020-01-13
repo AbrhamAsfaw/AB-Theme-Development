673de16643973ac525c5c832045f337e52ae2284
ABRHAMASFAW2018 <abrhamasfaw2018@gmail.com>
2020-01-13 18:20:57 +0300

    page templates

---
+++ template-parts/fullwidth.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ABwp
+ */
+
+get_header();
+
+while (have_posts()) {
+  the_post();
+
+ ?>    
+
+<br>
+    <div id="post-container">
+    
+      <section id="blogpost ">
+
+        <div class="card fullwidth">
+          
+          <?php if(has_post_thumbnail()) {  ?>
+
+          <div class="card-image">
+            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
+          </div>
+
+          <?php } ?>
+
+          <div class="card-description">
+            <?php the_content(); ?>
+          </div>
+
+        </div>
+
+      </section>
+
+      <?php } ?>
+
+    </div>
+    <br>
+
+<?php get_footer(); ?>    
---
+++ template-parts/sidebarcontent.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: Left-sidebar with Content Page
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ABwp
+ */
+
+get_header();
+
+while (have_posts()) {
+  the_post();
+
+ ?>    
+
+<br>
+    <div id="post-container">
+
+      <aside id="sidebar">
+        <?php dynamic_sidebar('sidebar-1'); ?>
+      </aside>
+    
+      <section id="blogpost">
+
+        <div class="card">
+          
+          <?php if(has_post_thumbnail()) {  ?>
+
+          <div class="card-image">
+            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
+          </div>
+
+          <?php } ?>
+
+          <div class="card-description">
+            <?php the_content(); ?>
+          </div>
+
+        </div>
+
+      </section>
+
+      <?php } ?>
+
+    </div>
+    <br>
+
+<?php get_footer(); ?>    
